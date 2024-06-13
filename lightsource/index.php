9545bbe2efc048e914d14bad3cfbf3083fe933f9
Graham Dunsby <gdunsby@gmail.com>
2024-06-13 13:44:17 +1000

    Initial commit

---
+++ lightsource/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1 class="highlighted-text-shadow">
+        CSS<br>
+        TEXT<br>
+        SHADOW<br>
+        HIGHLIGHT
+    </h1>
+
+    <small>That <code>2px</code> (almost) white highlight on the top of the text makes it seem as though there's a light source.</small>
+</body>
+
+</html>
---
+++ scroll/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+
+
+</head>
+
+<body>
+
+    <div class="container">
+
+        <div class="scroll-watcher"></div>
+        <article class="content-grid">
+            <header class="article-header">
+                <div class="header-content">
+                    <h1 class="article-title">Sroll Things</h1>
+                    <p>Scroll driven animation bollocks Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis libero in blanditiis nostrum vel perspiciatis quibusdam consectetur, ratione iusto ullam praesentium veniam nulla architecto nam. Possimus sapiente hic error excepturi, eligendi numquam, animi commodi voluptatum eos porro quas fugit corrupti?</p>
+                    <img class="header-image" src="https://picsum.photos/800/500" alt="dog">
+                </div>
+
+            </header>
+            <h2> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum quasi corrupti magnam accusantium ipsam dolorum doloremque nisi officiis debitis sunt architecto incidunt delectus asperiores maiores cupiditate, deleniti voluptas hic?</h2>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi alias, id aspernatur asperiores aliquam possimus, at vel ipsum blanditiis ducimus facere. Ad quidem, nihil animi at a sit? Dolorem facere laudantium perspiciatis assumenda nostrum nesciunt autem culpa dolorum voluptate ea?</p>
+        </article>
+
+        <div class="lorem">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis alias obcaecati nam autem maiores repellat doloribus quas, accusamus fugiat vero reiciendis id eaque nesciunt. Tempore debitis dolorum vel placeat temporibus nesciunt, culpa facilis architecto nulla molestiae Aut dolorem doloremque doloribus porro esse optio dicta necessitatibus magni, quisquam commodi vel, labore libero. Quae fugit, ipsam minus numquam quis recusandae accusantium nemo quidem, voluptatibus atque, esse iste delectus similique. Eligendi harum quo nihil iusto totam accusamus.
+        </div>
+        <article>
+            <img src="https://picsum.photos/800/500" alt="dog">
+        </article>
+        <div class="lorem">
+            s dolorum vel placeat temporibus nesciunt, culpa facilis architecto nulla molestiae Aut dolorem doloremque doloribus porro esse optio dicta necessitatibus magni, quisquam commodi vel, labore libero. Quae fugit, ipsam minus numquam quis recusandae s dolorum vel placeat temporibus nesciunt, culpa facilis architecto nulla molestiae Aut dolorem doloremque doloribus porro esse optio dicta necessitatibus magni, quisquam commodi vel, labore libero. Quae fugit, ipsam minus numquam quis recusandae s dolorum vel placeat temporibus nesciunt, culpa facilis architecto nulla molestiae Aut dolorem doloremque doloribus porro esse optio dicta necessitatibus magni, quisquam commodi vel, labore libero. Quae fugit, ipsam minus numquam quis recusandae s dolorum vel placeat temporibus nesciunt, culpa facilis architecto nulla molestiae Aut dolorem doloremque doloribus porro esse optio dicta necessitatibus magni, quisquam commodi vel, labore libero. Quae fugit, ipsam minus numquam quis recusandae s dolorum vel placeat temporibus nesciunt, culpa facilis architecto nulla molestiae Aut dolorem doloremque doloribus porro esse optio dicta necessitatibus magni, quisquam commodi vel, labore libero. Quae fugit, ipsam minus numquam quis recusandae
+        </div>
+
+    </div>
+</body>
+
+</html>
