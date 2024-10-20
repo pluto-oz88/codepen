051b29d1132260ca2961117e961f97c50d5d4ebe
Pluto <gdunsby@gmail.com>
2024-10-20 18:24:08 +1000

    Added external links

---
+++ extmenu/extmenu.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="extmenu.css">
+</head>
+<body>
+<div class="table_component" role="region" tabindex="0">
+<table>
+    <caption>Table 1</caption>
+    <thead>
+        <tr>
+            <th>No.</th>
+            <th>Feature</th>
+            <th>Explanation</th>
+            <th>Link</th>
+        </tr>
+    </thead>
+    <tbody>
+      <tr>
+            <td>0</td>
+            <td>aaalist</td>
+            <td>Full site list</td>
+            <td><a href="http://aaalist/">aaalist</a></td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>fading menu</td>
+            <td>Menu that fades all other links when highlight the selected</td>
+            <td><a href="http://aldevanddec/">aldevanddec</a></td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>nice square menu</td>
+            <td>Classic menu with opaque drop downs/td>
+            <td><a href="http://aerocon1/">aerocon1</a></td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>fading menu</td>
+            <td>Menu that fades all other links when highlight the selected</td>
+            <td><a href="http://aldevanddec/">aldevanddec</a></td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>fading menu</td>
+            <td>Menu that fades all other links when highlight the selected</td>
+            <td><a href="http://aldevanddec/">aldevanddec</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+<div style="margin-top:8px">Made with <a href="https://www.htmltables.io/" target="_blank">HTML Tables</a></div>
+</div>
+  
+</body>
+</html>
