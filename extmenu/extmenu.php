1ab8b3c73491200633d0d9337071b07deec846ec
Pluto <gdunsby@gmail.com>
2024-10-21 01:09:19 +1000

    Created external things

--- extmenu/extmenu.php
+++ extmenu/extmenu.php
@@ -1,94 +1,149 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="extmenu.css">
 </head>
+
 <body>
-<div class="table_component" role="region" tabindex="0">
-<table>
-    <caption>Table 1</caption>
-    <thead>
+  <div class="table_component" role="region" tabindex="0">
+    <table>
+      <caption>Web Site Features</caption>
+      <thead>
         <tr>
-            <th>No.</th>
-            <th>Feature</th>
-            <th>Explanation</th>
-            <th>Link</th>
+          <th>No.</th>
+          <th>Feature</th>
+          <th>Explanation</th>
+          <th>Link</th>
         </tr>
-    </thead>
-    <tbody>
-      <tr>
-            <td>0</td>
-            <td>aaalist</td>
-            <td>Full site list</td>
-            <td><a href="http://aaalist/">aaalist</a></td>
-        </tr>
+      </thead>
+      <tbody>
         <tr>
-            <td>1</td>
-            <td>fading menu</td>
-            <td>Menu that fades all other links when highlight the selected</td>
-            <td><a href="http://aldevanddec/">aldevanddec</a></td>
+          <td>0</td>
+          <td>aaalist</td>
+          <td>Full site list</td>
+          <td><a href="http://aaalist/">aaalist</a></td>
         </tr>
         <tr>
-            <td>3</td>
-            <td>nice square menu</td>
-            <td>Classic menu with opaque drop downs/td>
-            <td><a href="http://aerocon1/">aerocon1</a></td>
+          <td>1</td>
+          <td>fading menu</td>
+          <td>Menu that fades all other links when highlight the selected</td>
+          <td><a href="http://aldevanddec/">aldevanddec</a></td>
         </tr>
         <tr>
-            <td>4</td>
-            <td>fading menu</td>
-            <td>Menu that fades all other links when highlight the selected</td>
-            <td><a href="http://aldevanddec/">aldevanddec</a></td>
+          <td>2</td>
+          <td>nice square menu</td>
+          <td>Classic menu with opaque drop downs and good timer</td>
+          <td><a href="http://aerocon1/">aerocon1</a></td>
         </tr>
         <tr>
-            <td>5</td>
-            <td>fading menu</td>
-            <td>Menu that fades all other links when highlight the selected</td>
-            <td><a href="http://aldevanddec/">aldevanddec</a></td>
+          <td>3</td>
+          <td>Parallax image</td>
+          <td>Parallax images with transparent windows on hover</td>
+          <td><a href="http://aerogrow1/">Aerogrow1</a></td>
         </tr>
         <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+          <td>4</td>
+          <td>Classic Menu 2</td>
+          <td>Nice standard menu with transparent dropdowns</td>
+          <td><a href="http://aerogrow2/">Aerogrow2</a></td>
         </tr>
         <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+          <td>5</td>
+          <td>Sticky menu</td>
+          <td>Nice stick menu on scroll up with immediate show when scrolling down</td>
+          <td><a href="http://bestof/">Bestof</a></td>
         </tr>
         <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+          <td>6</td>
+          <td>Flying Birds</td>
+          <td>Birds flying over a lanscape</td>
+          <td><a href="http://clever/flying birds/birds2.html">Birds</a></td>
         </tr>
         <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+          <td>7</td>
+          <td>Nice color panes</td>
+          <td>Transparent panes with mouseover color change</td>
+          <td><a href="http://final1502/">Final1502</a></td>
         </tr>
         <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+          <td>8</td>
+          <td>Responsive Menu</td>
+          <td>responsive grid with menu too</td>
+          <td><a href="http://grid1502/">Grid1502</a></td>
+        </tr>
+
+        <tr>
+          <td>9</td>
+          <td>ICS</td>
+          <td>Calendar ICS Creation</td>
+          <td><a href="http://ics/">ICS</a></td>
+        </tr>
+
+        <tr>
+          <td>10</td>
+          <td>Modal example</td>
+          <td>Clean modal example = click outside to close</td>
+          <td><a href="http://modal/">Modal</a></td>
+        </tr>
+
+        <tr>
+          <td>11</td>
+          <td>One liners</td>
+          <td>One liner Grid and Flexbox examples</td>
+          <td><a href="http://oneliner/">Oneliner</a></td>
         </tr>
         <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+          <td>12</td>
+          <td>Orientation</td>
+          <td>Device Orientation specific</td>
+          <td><a href="http://orientation/">Orientation</a></td>
         </tr>
-    </tbody>
-</table>
-<div style="margin-top:8px">Made with <a href="https://www.htmltables.io/" target="_blank">HTML Tables</a></div>
-</div>
-  
+
+        <tr>
+          <td>13</td>
+          <td>image resize</td>
+          <td>jpg, png etc resizer</td>
+          <td><a href="http://resize/">resize</a></td>
+        </tr>
+
+        <tr>
+          <td>14</td>
+          <td>Buttons and behaviours</td>
+          <td>Buttons and various effects with grid</td>
+          <td><a href="http://revolution/">Revolution</a></td>
+        </tr>
+        <tr>
+          <td>15</td>
+          <td>Scrolling js</td>
+          <td>Various Scroll behaviors javascripts</td>
+          <td><a href="http://scrollmaster/">Scrollmaster</a></td>
+        </tr>
+
+        <tr>
+          <td>16</td>
+          <td>Swiper</td>
+          <td>Swiper for touch screen devices</td>
+          <td><a href="http://swiper/">Swiper</a></td>
+        </tr>
+        <tr>
+          <td>17</td>
+          <td>Tab template with links</td>
+          <td>Tabbed document with links to any tab</td>
+          <td><a href="http://tabult">Tabult</a></td>
+        </tr>
+
+        <tr>
+          <td>18</td>
+          <td>Z Index </td>
+          <td>Z-Index examples</td>
+          <td><a href="http://zindex">Zindex</a></td>
+        </tr>
+      </tbody>
+    </table>
 </body>
+
 </html>
