283b69ace481fd0e3db564d3f8644f54b2ae92db
Pluto <gdunsby@gmail.com>
2024-11-25 11:14:21 +1000

    big update

---
+++ textA/textA.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="textA.css">
+</head>
+<div class="page-layout">
+  <section class="faq">
+    <div>
+      <h2 class="section-style">Animated summary and details</h2>
+    </div>
+    <div class="questions-and-answers">
+      <details>
+        <summary>&nbsp;Can I animate details/summary elements?</summary>
+        <p>This used to be difficult. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae fugit voluptatem architecto aut quia blanditiis tempora rem ad porro temporibus voluptate odit incidunt atque, molestias unde natus dicta vel sint.</p>
+      </details>
+      <details>
+        <summary>&nbsp;&nbsp;&nbsp;&nbsp;Can I nominate details/summary elements?</summary>
+        <p>This used to be impossible. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias laudantium molestias assumenda voluptatum porro ab, quasi amet at optio labore.</p>
+        <details>
+        <summary>Inner Sanctum</summary>
+        <p>Will it work?</p>
+        </details>
+      </details>
+      <details>
+        <summary>Can I designate details/summary elements?</summary>
+        <p>This used to be fucking hard. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus fugiat, quas beatae odit numquam nam, explicabo nobis porro eum alias autem rerum atque iste enim!</p>
+      </details>
+      <details>
+        <summary>Can I automate details/summary elements?</summary>
+        <p>This used to be easy not difficult. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore explicabo exercitationem deserunt ipsam pariatur adipisci suscipit quos?</p>
+      </details>
+
+
+    </div>
+  </section>
+</div>
+
+<body>
+
+</body>
+
+</html>
