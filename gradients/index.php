a4d6835db229ceec017f8a7a7784a9de683fae40
Pluto <gdunsby@gmail.com>
2024-12-01 20:37:30 +1000

    added modules

--- gradients/index.php
+++ gradients/index.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="gradients.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="gradients.css">
 </head>
+
 <body>
-<div class="card card--1"></div>
+  <div class="card card--1">card 1</div>
 
-<div class="card card--2"></div>
+  <div class="card card--2">card 2</div>
 
-<div class="card card--3"></div>
+  <div class="card card--3">card 3</div>
 
-<div class="card card--4"></div>
+  <div class="card card--4">card 4</div>
 
-<div class="card card--5"></div>
+  <div class="card card--5">card 5</div>
 </body>
+
 </html>
